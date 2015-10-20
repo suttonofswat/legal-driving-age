3e85562191bbaf983dbd13f118907cde3f935c65
Leslie Sutton <l4sutton@gmail.com>
2015-10-20 13:33:15 -0500

    completed asking if 16, older or if it is a negative number

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+window.$ = require('jquery');
+window.jQuery = $;
+
+
+$(document).ready(function() {
+	var $ageInput = $('#ageInput');
+	var $button = $('#button');
+	var $feedback = $('#feedback');
+
+	$button.on('click', function(e){
+		e.preventDefault();
+		var $newAge = $ageInput.val();
+
+		$newAge >= 16 ? $feedback.text('you are old enough to legally drive.')
+		: $newAge <= 0 ? $feedback.text('please enter a valid age')
+		: $feedback.text('you are not old enough to legally drive');;
+
+	})
+
+
+
+})
+
+
+
+
